15d3b6bfda1b5eea31ddd78f4362b42117187ca1
Sushant <patrikarss@rknec.edu>
2020-03-17 23:08:50 +0530

    adding cactus

---
+++ cactus.py
@@ -0,0 +1,10 @@
+import pygame
+from game import Game
+class Cactus:
+    def __init__(self):
+        self.x = Game.width
+        self.cactus  = pygame.image.load('img/cactus.png').convert_alpha()
+        self.rect = self.cactus.get_rect()
+
+    def showCactus(self):
+        Game.display.blit(self.cactus,[self.x,Game.height-50-self.rect.height])
