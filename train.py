69c7e419e1c215034412dbf8336393a9d6d0e165
Sushant <patrikarss@rknec.edu>
2020-03-19 13:14:14 +0530

    added functionality to save weights

--- train.py
+++ train.py
@@ -2,9 +2,10 @@ import cv2
 import pandas as pd
 import time
 import numpy as np
-from keras import Sequential
-from keras.layers import Conv2D,MaxPool2D,Dense,Flatten
+from keras.models import Sequential
+from keras.layers import Dense,Conv2D,Flatten,MaxPool2D
 from keras.utils import to_categorical
+from PIL import Image
 global df
 df = pd.DataFrame(columns=['Image','Action'])
 
@@ -21,6 +22,9 @@ def capture_do_nothing():
         if cv2.waitKey(1) & 0xFF == ord('s'):
             while count != 50:
                 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+                gray = Image.fromarray(gray)
+                gray = gray.resize((50,50))
+                gray = np.array(gray)
                 df = df.append({'Image':gray,'Action':0},ignore_index=True)
                 count+=1
                 return_value, image = camera.read()
@@ -41,6 +45,9 @@ def capture_jump():
         if cv2.waitKey(1) & 0xFF == ord('s'):
             while count != 50:
                 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                gray = Image.fromarray(gray)
+                gray = gray.resize((50, 50))
+                gray = np.array(gray)
                 df = df.append({'Image': gray, 'Action': 1}, ignore_index=True)
                 count+=1
                 return_value, image = camera.read()
@@ -57,14 +64,14 @@ def prepare_dataset():
     for i in range(100):
         x.append(X[i])
     x = np.asarray(x)
-    x = (x-128)/128
-    x = np.reshape(x,(100,480,640,1))
+    x = (x.astype(float)-128)/128
+    x = np.reshape(x,(100,50,50,1))
     y = to_categorical(Y)
     return x,y
 
 def load_model():
     model = Sequential()
-    model.add(Conv2D(32,kernel_size=(3,3),strides=(1,1),activation='relu',input_shape=(480,640,1)))
+    model.add(Conv2D(32,kernel_size=(3,3),strides=(1,1),activation='relu',input_shape=(50,50,1)))
     model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
     model.add(Conv2D(64,kernel_size=(3,3),strides=(1,1),activation='relu'))
     model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
@@ -74,7 +81,10 @@ def load_model():
     model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
     return model
 def train(model,X,y):
-    model.fit(X,y,batch_size=16,epochs=50)
+    model.fit(X,y,batch_size=16,epochs=10)
+
+def save_model(model):
+    model.save_weights('weights.h5')
 
 if __name__=='__main__':
     capture_do_nothing()
@@ -84,3 +94,4 @@ if __name__=='__main__':
     X,y = prepare_dataset()
     model = load_model()
     train(model,X,y)
+    save_model(model)
