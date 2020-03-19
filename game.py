063081f1fe6c0c2b5645241e105d457c8d792635
Sushant <patrikarss@rknec.edu>
2020-03-19 15:07:47 +0530

    added cnn

--- game.py
+++ game.py
@@ -1,5 +1,11 @@
 import pygame
 import random
+import cv2
+from PIL import Image
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense,Conv2D,Flatten,MaxPool2D
+
 class Game:
     pygame.init()
     clock = pygame.time.Clock()
@@ -16,7 +22,11 @@ class Game:
     cactuses = []
     counter = 0
     y = 50
+    camera = cv2.VideoCapture(0)
+    exit = False
     def __init__(self):
+        self.model = self.load_model()
+        self.model.load_weights('weights.h5')
         self.gameloop()
     def gameloop(self):
         while not self.gameExit:
@@ -25,9 +35,6 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.gameExit = True
-                if event.type==pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE and self.y==50:
-                        self.y +=150
             if self.counter%(random.randrange(100,110))==0:
                 from cactus import Cactus
                 self.cactuses.append(Cactus())
@@ -36,6 +43,14 @@ class Game:
                 c.x-=15
                 if c.x <= -c.rect.width:
                     self.cactuses = self.cactuses[1:]
+            pic = self.take_photo()
+            result = self.predict(pic)
+            print(result)
+            if result==0:
+                pass
+            else:
+                if self.y==50:
+                    self.y+=150
             self.display.blit(self.dino, [10, self.height-self.y-self.dino_rect.height])
             pygame.display.update()
             if self.y>50:
@@ -44,6 +59,34 @@ class Game:
             self.counter+=1
         pygame.quit()
         quit()
+
+    def take_photo(self):
+        return_value, image = self.camera.read()
+        cv2.imshow('image', image)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = Image.fromarray(gray)
+        gray = gray.resize((50, 50))
+        gray = np.array(gray)
+        gray = np.resize(gray, (1, 50, 50, 1))
+        (gray.astype(float) - 128) / 128
+        return gray
+
+    def load_model(self):
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), activation='relu', input_shape=(50, 50, 1)))
+        model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
+        model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Flatten())
+        model.add(Dense(512, activation='relu'))
+        model.add(Dense(2, activation='softmax'))
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        return model
+
+    def predict(self,pic):
+        prob = self.model.predict(pic)
+        result = np.argmax(prob)
+        return result
 
 if __name__=='__main__':
     g = Game()
