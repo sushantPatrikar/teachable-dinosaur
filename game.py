60841efd09f928b4ffca59d5166402013d8326d4
Sushant <patrikarss@rknec.edu>
2020-03-19 16:51:42 +0530

    reformatted code

--- game.py
+++ game.py
@@ -4,16 +4,17 @@ import cv2
 from PIL import Image
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense,Conv2D,Flatten,MaxPool2D
+from keras.layers import Dense, Conv2D, Flatten, MaxPool2D
+
 
 class Game:
     pygame.init()
     clock = pygame.time.Clock()
     width = 1024
-    height = 700
-    black = (0,0,0)
-    white = (255,255,255)
-    display = pygame.display.set_mode((width,height))
+    height = 500
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    display = pygame.display.set_mode((width, height))
     gameExit = False
     dino = pygame.image.load('img/dino.png').convert_alpha()
     cactus = pygame.image.load('img/cactus.png').convert_alpha()
@@ -24,10 +25,12 @@ class Game:
     y = 50
     camera = cv2.VideoCapture(0)
     exit = False
+
     def __init__(self):
         self.model = self.load_model()
         self.model.load_weights('weights.h5')
         self.gameloop()
+
     def gameloop(self):
         while not self.gameExit:
             self.display.fill(self.white)
@@ -35,28 +38,27 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.gameExit = True
-            if self.counter%(random.randrange(100,110))==0:
+            if self.counter % (random.randrange(100, 110)) == 0:
                 from cactus import Cactus
                 self.cactuses.append(Cactus())
             for c in self.cactuses:
                 c.showCactus()
-                c.x-=15
+                c.x -= 15
                 if c.x <= -c.rect.width:
                     self.cactuses = self.cactuses[1:]
             pic = self.take_photo()
             result = self.predict(pic)
-            print(result)
-            if result==0:
+            if result == 0:
                 pass
             else:
-                if self.y==50:
-                    self.y+=150
-            self.display.blit(self.dino, [10, self.height-self.y-self.dino_rect.height])
+                if self.y == 50:
+                    self.y += 150
+            self.display.blit(self.dino, [10, self.height - self.y - self.dino_rect.height])
             pygame.display.update()
-            if self.y>50:
-                self.y-=10
+            if self.y > 50:
+                self.y -= 10
             self.clock.tick(30)
-            self.counter+=1
+            self.counter += 1
         pygame.quit()
         quit()
 
@@ -79,14 +81,15 @@ class Game:
         model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
         model.add(Flatten())
         model.add(Dense(512, activation='relu'))
-        model.add(Dense(2, activation='softmax'))
+        model.add(Dense(2, activation='sigmoid'))
         model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
         return model
 
-    def predict(self,pic):
+    def predict(self, pic):
         prob = self.model.predict(pic)
         result = np.argmax(prob)
         return result
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     g = Game()
