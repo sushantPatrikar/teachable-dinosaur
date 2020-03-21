af38f210d60abb1f317c754d3e5605e95cf23cf3
Sushant <patrikarss@rknec.edu>
2020-03-22 00:53:13 +0530

    added intro screen

--- game.py
+++ game.py
@@ -3,8 +3,9 @@ import random
 import cv2
 from PIL import Image
 import numpy as np
-from keras.models import Sequential
+from keras.models import Sequential,Model
 from keras.layers import Dense, Conv2D, Flatten, MaxPool2D,Dropout,MaxPooling2D
+
 
 
 class Game:
@@ -29,8 +30,26 @@ class Game:
     def __init__(self):
         self.model = self.load_model()
         self.model.load_weights('weights.h5')
-        self.gameloop()
+        self.intro()
 
+    def intro(self):
+        intro = True
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type ==pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.gameloop()
+            self.display.fill(self.white)
+            largeText = pygame.font.Font('freesansbold.ttf', 100)
+            textSurf = largeText.render("Press SPACE to play",True,self.black)
+            textRect = textSurf.get_rect()
+            textRect.center = ((self.width / 2), (self.height / 2))
+            self.display.blit(textSurf, textRect)
+            pygame.display.update()
+            self.clock.tick(30)
     def gameloop(self):
         while not self.gameExit:
             self.display.fill(self.white)
@@ -63,14 +82,15 @@ class Game:
         quit()
 
     def take_photo(self):
+        cv2.resizeWindow('image', 300, 350)
         return_value, image = self.camera.read()
         cv2.imshow('image', image)
-        # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        im = Image.fromarray(image)
+        im = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        im = Image.fromarray(im)
         im = im.resize((100, 100))
         im = np.array(im)
         im = np.resize(im, (1, 100, 100, 3))
-        (im.astype(float) - 128) / 128
+        im = (im.astype(float) - 128) / 128
         return im
 
     def load_model(self):
@@ -79,12 +99,9 @@ class Game:
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
         model.add(Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-        model.add(Conv2D(128, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
         model.add(Flatten())
-        # model.add(Dense(512, activation='sigmoid'))
-        model.add(Dense(1024, activation='sigmoid'))
-        model.add(Dropout(0.6))
+        model.add(Dense(1000, activation='sigmoid'))
+        model.add(Dropout(0.4))
         model.add(Dense(2, activation='sigmoid'))
         model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
         return model
